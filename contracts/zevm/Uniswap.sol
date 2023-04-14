75df8431d9fc2307790fdbe097baa95ff7a806b0
Denis Fadeev <denis@fadeev.org>
2023-04-14 15:43:53 +0400

    fix: remove uniswap router import

--- contracts/zevm/Uniswap.sol
+++ contracts/zevm/Uniswap.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 
