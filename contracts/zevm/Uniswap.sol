4a8530ae2e0b454879c91fbe446df1582b0b3d8e
Denis Fadeev <denis@fadeev.org>
2023-04-14 15:27:06 +0400

    Added uniswap

---
+++ contracts/zevm/Uniswap.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+
+contract UniswapImports {}
