c947f8f39343d6a2b877fea057131668f304a714
Denis Fadeev <denis@fadeev.org>
2024-02-02 17:17:15 +0400

    lint

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,12 +3,11 @@ import "@nomiclabs/hardhat-etherscan";
 import "@typechain/hardhat";
 import "tsconfig-paths/register";
 import "hardhat-abi-exporter";
+import "./tasks/addresses";
 
 import { getHardhatConfigNetworks } from "@zetachain/networks";
 import * as dotenv from "dotenv";
 import type { HardhatUserConfig } from "hardhat/types";
-
-import "./tasks/addresses";
 
 dotenv.config();
 
@@ -31,22 +30,22 @@ const config: HardhatUserConfig = {
       { version: "0.4.19" /** For zevm/wzeta.sol */ },
       { version: "0.5.10" /** For create2 factory */ },
       {
-        version: "0.5.16",
         settings: {
           optimizer: {
             enabled: true,
             runs: 999999,
           },
         },
+        version: "0.5.16",
       },
       {
-        version: "0.6.6",
         settings: {
           optimizer: {
             enabled: true,
             runs: 999999,
           },
-        } /** For uniswap v2 */,
+        },
+        version: "0.6.6" /** For uniswap v2 */,
       },
       { version: "0.7.6" /** For uniswap v3 */ },
       { version: "0.8.7" },
