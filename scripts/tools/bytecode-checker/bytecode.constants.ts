4fd776a514d505543fa01c58ae7986776c37471e
Andres Martin Aiello <50411235+andresaiello@users.noreply.github.com>
2024-03-01 12:49:05 -0300

    feat: Create script to verify bytecode (#127)

---
+++ scripts/tools/bytecode-checker/bytecode.constants.ts
@@ -0,0 +1,18 @@
+export const ETHERSCAN_API_ENDPOINT = "https://api.etherscan.io";
+export const BSC_ETHERSCAN_API_ENDPOINT = "https://api.bscscan.com";
+export const ZETA_NODE_ENDPOINT = "http://100.71.167.102:8545";
+
+export const etherscanApiKey = process.env.ETHERSCAN_API_KEY;
+export const bscEtherscanApiKey = process.env.BSCSCAN_API_KEY;
+
+export const ethConnectorAddress = "0x000007Cf399229b2f5A4D043F20E90C9C98B7C6a";
+export const ethERC20CustodyAddress = "0x000001b91C19A31809e769110d35FAd2C15BCeA7";
+export const ethTokenAddress = "0xf091867EC603A6628eD83D274E835539D82e9cc8";
+
+export const bscConnectorAddress = "0x000063A6e758D9e2f438d430108377564cf4077D";
+export const bscERC20CustodyAddress = "0x0000006Abbf11Ed0FabFD247f1F4d76A383cC395";
+export const bscTokenAddress = "0x0000028a2eB8346cd5c0267856aB7594B7a55308";
+
+export const BTC_BTC = "0x13A0c5930C028511Dc02665E7285134B6d11A5f4";
+export const BNB_BSC = "0x48f80608B672DC30DC7e3dbBd0343c5F02C738Eb";
+export const ETH_ETH = "0xd97B1de3619ed2c6BEb3860147E30cA8A7dC9891";
