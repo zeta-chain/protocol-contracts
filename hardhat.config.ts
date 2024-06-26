b195e246fa35edcfd9c69545ce0c5a4c8d890c8d
Lucas Bertrand <lucas.bertrand.22@gmail.com>
2024-06-26 09:40:35 +0200

    chore: add `yarn coverage` to generate coverage report (#170)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,7 @@ import "@nomicfoundation/hardhat-verify";
 import "@typechain/hardhat";
 import "tsconfig-paths/register";
 import "hardhat-abi-exporter";
+import "solidity-coverage";
 import "hardhat-gas-reporter";
 import "./tasks/addresses";
 
