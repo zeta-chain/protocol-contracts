0bbea72e8a514b6bdd97be9d1401a14b01b2714e
skosito <skostic9242@gmail.com>
2024-06-27 02:23:23 +0200

    generate

--- test/prototypes/GatewayZEVM.spec.ts
+++ test/prototypes/GatewayZEVM.spec.ts
@@ -1,11 +1,12 @@
-import { expect } from "chai";
-import { BigNumber, Contract } from "ethers";
-import { ethers, upgrades } from "hardhat";
 import { AddressZero } from "@ethersproject/constants";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { SystemContract, ZRC20 } from "@typechain-types";
-import { FUNGIBLE_MODULE_ADDRESS } from "../test.helpers";
+import { expect } from "chai";
+import { BigNumber, Contract } from "ethers";
 import { parseEther } from "ethers/lib/utils";
+import { ethers, upgrades } from "hardhat";
+
+import { FUNGIBLE_MODULE_ADDRESS } from "../test.helpers";
 const hre = require("hardhat");
 
 describe("GatewayZEVM inbound", function () {
@@ -49,44 +50,65 @@ describe("GatewayZEVM inbound", function () {
 
     const Gateway = await ethers.getContractFactory("GatewayZEVM");
     gateway = await upgrades.deployProxy(Gateway, [], {
-        initializer: "initialize",
-        kind: "uups",
+      initializer: "initialize",
+      kind: "uups",
     });
 
     await ZRC20Contract.connect(owner).approve(gateway.address, parseEther("100"));
+  });
+
+  it("should withdraw zrc20 and emit event", async function () {
+    const tx = await gateway
+      .connect(owner)
+      .withdraw(ethers.utils.arrayify(addrs[0].address), parseEther("1"), ZRC20Contract.address);
+
+    const balanceOfAfterWithdrawal = (await ZRC20Contract.balanceOf(owner.address)) as BigNumber;
+    expect(balanceOfAfterWithdrawal).to.equal(parseEther("99"));
+
+    await expect(tx)
+      .to.emit(gateway, "Withdrawal")
+      .withArgs(
+        ethers.utils.getAddress(owner.address),
+        addrs[0].address.toLowerCase(),
+        parseEther("1"),
+        0,
+        await ZRC20Contract.PROTOCOL_FLAT_FEE(),
+        "0x"
+      );
+  });
+
+  it("should withdraw zrc20 and call and emit event with message", async function () {
+    let ABI = ["function hello(address to)"];
+    let iface = new ethers.utils.Interface(ABI);
+    const message = iface.encodeFunctionData("hello", ["0x1234567890123456789012345678901234567890"]);
+
+    const tx = await gateway
+      .connect(owner)
+      .withdrawAndCall(ethers.utils.arrayify(addrs[0].address), parseEther("1"), ZRC20Contract.address, message);
+
+    const balanceOfAfterWithdrawal = (await ZRC20Contract.balanceOf(owner.address)) as BigNumber;
+    expect(balanceOfAfterWithdrawal).to.equal(parseEther("99"));
+
+    await expect(tx)
+      .to.emit(gateway, "Withdrawal")
+      .withArgs(
+        ethers.utils.getAddress(owner.address),
+        addrs[0].address.toLowerCase(),
+        parseEther("1"),
+        0,
+        await ZRC20Contract.PROTOCOL_FLAT_FEE(),
+        message
+      );
+  });
+
+  it("should call and emit event without asset transfer", async function () {
+    let ABI = ["function hello(address to)"];
+    let iface = new ethers.utils.Interface(ABI);
+    const message = iface.encodeFunctionData("hello", ["0x1234567890123456789012345678901234567890"]);
+
+    const tx = await gateway.connect(owner).call(ethers.utils.arrayify(addrs[0].address), message);
+    await expect(tx)
+      .to.emit(gateway, "Call")
+      .withArgs(ethers.utils.getAddress(owner.address), addrs[0].address.toLowerCase(), message);
+  });
 });
-
-    it("should withdraw zrc20 and emit event", async function () {
-        const tx = await gateway.connect(owner).withdraw(ethers.utils.arrayify(addrs[0].address), parseEther("1"), ZRC20Contract.address);
-
-        const balanceOfAfterWithdrawal = await ZRC20Contract.balanceOf(owner.address) as BigNumber;
-        expect(balanceOfAfterWithdrawal).to.equal(parseEther("99"));
-
-        await expect(tx).to.emit(gateway, "Withdrawal")
-            .withArgs(ethers.utils.getAddress(owner.address), addrs[0].address.toLowerCase(), parseEther("1"), 0, await ZRC20Contract.PROTOCOL_FLAT_FEE(), "0x");
-    }); 
-
-    it("should withdraw zrc20 and call and emit event with message", async function () {
-        let ABI = ["function hello(address to)"];
-        let iface = new ethers.utils.Interface(ABI);
-        const message = iface.encodeFunctionData("hello", ["0x1234567890123456789012345678901234567890"]);
-
-        const tx = await gateway.connect(owner).withdrawAndCall(ethers.utils.arrayify(addrs[0].address), parseEther("1"), ZRC20Contract.address, message);
-    
-        const balanceOfAfterWithdrawal = await ZRC20Contract.balanceOf(owner.address) as BigNumber;
-        expect(balanceOfAfterWithdrawal).to.equal(parseEther("99"));
-    
-        await expect(tx).to.emit(gateway, "Withdrawal")
-            .withArgs(ethers.utils.getAddress(owner.address), addrs[0].address.toLowerCase(), parseEther("1"), 0, await ZRC20Contract.PROTOCOL_FLAT_FEE(), message);
-        });
-
-    it("should call and emit event without asset transfer", async function() {
-        let ABI = ["function hello(address to)"];
-        let iface = new ethers.utils.Interface(ABI);
-        const message = iface.encodeFunctionData("hello", ["0x1234567890123456789012345678901234567890"]);
-
-        const tx = await gateway.connect(owner).call(ethers.utils.arrayify(addrs[0].address), message);
-        await expect(tx).to.emit(gateway, "Call")
-         .withArgs(ethers.utils.getAddress(owner.address), addrs[0].address.toLowerCase(), message);
-    });
-    });
