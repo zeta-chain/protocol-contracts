d6ddeadbba4d479365cbf130c1d1cbe4c8ef870e
Andres Aiello <aaiello@gmail.com>
2024-06-28 12:41:40 -0300

    lint

--- lib/types.ts
+++ lib/types.ts
@@ -1,4 +1,40 @@
-export type ParamSymbol = "USDC.BSC" | "USDC.ETH" | "BTC.BTC" | "BNB.BSC" | "USDT.ETH" | "USDT.BSC" | "ETH.ETH" | "sETH.SEPOLIA" | "USDC" | "gETH" | "tMATIC" | "tBTC" | "USDC.SEPOLIA" | "tBNB";
-export type ParamChainName = "eth_mainnet" | "bsc_mainnet" | "zeta_mainnet" | "btc_mainnet" | "bsc_testnet" | "zeta_testnet" | "btc_testnet" | "sepolia_testnet";
-export type ParamType = "connector" | "erc20Custody" | "pauser" | "tss" | "tssUpdater" | "uniswapV2Factory" | "uniswapV2Router02" | "uniswapV3Factory" | "uniswapV3Router" | "weth9" | "zetaToken" | "fungibleModule" | "systemContract" | "zrc20" | "zetaTokenConsumerUniV3";
-
+export type ParamSymbol =
+  | "BNB.BSC"
+  | "BTC.BTC"
+  | "ETH.ETH"
+  | "gETH"
+  | "sETH.SEPOLIA"
+  | "tBNB"
+  | "tBTC"
+  | "tMATIC"
+  | "USDC.BSC"
+  | "USDC.ETH"
+  | "USDC.SEPOLIA"
+  | "USDC"
+  | "USDT.BSC"
+  | "USDT.ETH";
+export type ParamChainName =
+  | "bsc_mainnet"
+  | "bsc_testnet"
+  | "btc_mainnet"
+  | "btc_testnet"
+  | "eth_mainnet"
+  | "sepolia_testnet"
+  | "zeta_mainnet"
+  | "zeta_testnet";
+export type ParamType =
+  | "connector"
+  | "erc20Custody"
+  | "fungibleModule"
+  | "pauser"
+  | "systemContract"
+  | "tss"
+  | "tssUpdater"
+  | "uniswapV2Factory"
+  | "uniswapV2Router02"
+  | "uniswapV3Factory"
+  | "uniswapV3Router"
+  | "weth9"
+  | "zetaToken"
+  | "zetaTokenConsumerUniV3"
+  | "zrc20";
