2054b10e2e98b633939edc596e66f81e2275cd3d
Denis Fadeev <denis@fadeev.org>
2024-08-12 19:32:08 +0300

    ci: publish v2 (#297)

---
+++ v2/eslint.config.js
@@ -0,0 +1,13 @@
+module.exports = [
+  {
+    ignores: [".yarn", "artifacts", "cache", "dist", "node_modules", "typechain-types", "abi", "docs", "lib"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+      parser: require("@typescript-eslint/parser"),
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+    },
+  },
+];
