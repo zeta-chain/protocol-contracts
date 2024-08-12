5fabcd3f9f000fa0f8df8d4fee6fd2623ae81e6b
skosito <skostic9242@gmail.com>
2024-08-12 18:22:19 +0200

    fixes after merge

--- v2/test/utils/SenderZEVM.sol
+++ v2/test/utils/SenderZEVM.sol
@@ -76,13 +76,6 @@ contract SenderZEVM {
             revertMessage: ""
         });
 
-        RevertOptions memory revertOptions = RevertOptions({
-            revertAddress: address(0x321),
-            callOnRevert: true,
-            abortAddress: address(0x321),
-            revertMessage: ""
-        });
-
         // Pass encoded call to gateway
         IGatewayZEVM(gateway).withdrawAndCall(receiver, amount, zrc20, message, 1, revertOptions);
     }
