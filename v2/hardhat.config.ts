ed3ef6feebd353e3ab980fbb98b2588216fa4be8
Denis Fadeev <denis@fadeev.org>
2024-08-13 12:28:42 +0300

    add hardhat config

---
+++ v2/hardhat.config.ts
@@ -0,0 +1,9 @@
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [{ version: "0.8.26" }],
+  },
+};
+
+export default config;
