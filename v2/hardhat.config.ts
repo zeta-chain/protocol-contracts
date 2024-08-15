d38b7274c1bd961beccfc4c27c2dcff251e00fce
Denis Fadeev <denis@fadeev.org>
2024-08-15 11:06:11 +0300

    remove hardhat

--- v2/hardhat.config.ts
+++
@@ -1,9 +0,0 @@
-import { HardhatUserConfig } from "hardhat/config";
-
-const config: HardhatUserConfig = {
-  solidity: {
-    compilers: [{ version: "0.8.26" }],
-  },
-};
-
-export default config;
