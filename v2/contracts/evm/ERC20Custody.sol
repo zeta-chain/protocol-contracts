222aa33cd9606927073ffcbad118364666d2680f
jhuntbach-bc <jhuntbach@blockchain.com>
2024-12-31 10:21:09 +0000

    chore: more permissive solidity pragma

--- v2/contracts/evm/ERC20Custody.sol
+++ v2/contracts/evm/ERC20Custody.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
+pragma solidity ^0.8.26;
 
 import { IERC20Custody } from "./interfaces/IERC20Custody.sol";
 import { IGatewayEVM, MessageContext } from "./interfaces/IGatewayEVM.sol";
--- v2/contracts/evm/ZetaConnectorNative.sol
+++ v2/contracts/evm/ZetaConnectorNative.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
+pragma solidity ^0.8.26;
 
 import "./ZetaConnectorBase.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
