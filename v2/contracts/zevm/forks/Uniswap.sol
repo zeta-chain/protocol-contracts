d50d83a1bf31e2c61d535f3f09768a42b4ab7279
lumtis <lucas.bertrand.22@gmail.com>
2025-01-09 13:30:08 +0100

    move contracts

---
+++ v2/contracts/zevm/forks/Uniswap.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.5.16;
+
+import "@uniswap/v2-core/contracts/UniswapV2Factory.sol";
+import "@uniswap/v2-core/contracts/UniswapV2Pair.sol";
+
+contract UniswapImports { }
