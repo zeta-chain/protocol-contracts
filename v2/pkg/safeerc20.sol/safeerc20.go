a8e4d6de6c33c69cb1fa5440b4bb19dfa9cb6736
lumtis <lucas.bertrand.22@gmail.com>
2025-01-09 15:23:51 +0100

    generate

--- v2/pkg/safeerc20.sol/safeerc20.go
+++ v2/pkg/safeerc20.sol/safeerc20.go
@@ -32,7 +32,7 @@ var (
 // SafeERC20MetaData contains all meta data concerning the SafeERC20 contract.
 var SafeERC20MetaData = &bind.MetaData{
 	ABI: "[{\"type\":\"error\",\"name\":\"SafeERC20FailedDecreaseAllowance\",\"inputs\":[{\"name\":\"spender\",\"type\":\"address\",\"internalType\":\"address\"},{\"name\":\"currentAllowance\",\"type\":\"uint256\",\"internalType\":\"uint256\"},{\"name\":\"requestedDecrease\",\"type\":\"uint256\",\"internalType\":\"uint256\"}]},{\"type\":\"error\",\"name\":\"SafeERC20FailedOperation\",\"inputs\":[{\"name\":\"token\",\"type\":\"address\",\"internalType\":\"address\"}]}]",
-	Bin: "0x60566037600b82828239805160001a607314602a57634e487b7160e01b600052600060045260246000fd5b30600052607381538281f3fe73000000000000000000000000000000000000000030146080604052600080fdfea26469706673582212200ed0e26b77a0ea6e72b739b8801c91160a76e747bccb513fbf8ea46593f63b8664736f6c634300081a0033",
+	Bin: "0x60566037600b82828239805160001a607314602a57634e487b7160e01b600052600060045260246000fd5b30600052607381538281f3fe73000000000000000000000000000000000000000030146080604052600080fdfea26469706673582212205a486d6518f77cfaa095c2c7336066b2fd263362fb0a8d78cb1b5f51f6cf453164736f6c634300081a0033",
 }
 
 // SafeERC20ABI is the input ABI used to generate the binding from.
