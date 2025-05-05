4b8eacff7e2684d3266993bb2bddbe1693f8b9f9
s2imonovic <s2imonovic@gmail.com>
2025-05-05 13:17:18 +0200

    feat: rename ZetaETH and ZetaNonETH

--- contracts/evm/ZetaConnectorBase.sol
+++ contracts/evm/ZetaConnectorBase.sol
@@ -91,9 +91,11 @@ abstract contract ZetaConnectorBase is
 
         _revokeRole(WITHDRAWER_ROLE, tssAddress);
         _revokeRole(TSS_ROLE, tssAddress);
+        _revokeRole(PAUSER_ROLE, tssAddress);
 
         _grantRole(WITHDRAWER_ROLE, newTSSAddress);
         _grantRole(TSS_ROLE, newTSSAddress);
+        _grantRole(PAUSER_ROLE, newTSSAddress);
 
         emit UpdatedZetaConnectorTSSAddress(tssAddress, newTSSAddress);
 
