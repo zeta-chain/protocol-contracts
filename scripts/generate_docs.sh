be9ee173f232afe524bb26485e8260697cb365dd
Denis Fadeev <denis@fadeev.org>
2025-06-12 15:23:50 +0300

    table of contents

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 forge doc
 
-rm contracts.md
+rm -rf index.md
 
 cat docs/src/README.md docs/src/SUMMARY.md > docs/src/index.md
 
@@ -22,14 +22,19 @@ else
     find docs/src/ -type f -name "*.md" -exec sed -i -E 's|(https://github.com/zeta-chain/protocol-contracts/blob/)[^/]+/|\1main/|g' {} +
 fi
 
-echo -e "\n# Contract Documentation\n" > contracts.md
 find docs/src/contracts -type f -name "*.md" -print0 | sort -z | while IFS= read -r -d '' file; do
-    echo -e "\n## $(basename "$file" .md)\n" >> contracts.md
-    cat "$file" >> contracts.md
+    echo -e "\n## $(basename "$file" .md)\n" >> index.md
+    cat "$file" >> index.md
 done
 
 rm -rf docs
 
 mkdir -p docs
 
-mv contracts.md docs/contracts.md
+echo -e "# ZetaChain and EVM Protocol Contracts\n" > docs/index.md
+
+npx markdown-toc index.md >> docs/index.md
+
+cat index.md >> docs/index.md
+
+rm index.md
